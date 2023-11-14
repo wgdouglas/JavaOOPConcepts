13df6e60c1da5fa65460f753c6f22419028581de
gfizzle <winstongdouglas4@gmail.com>
2023-11-13 23:22:29 -0600

    This is the section covering classes & object set up & connections

---
+++ src/org/wgcodecompany/classes/Student.java
@@ -0,0 +1,60 @@
+package org.wgcodecompany.classes;
+
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.Value;
+
+//@Value
+//@Builder
+//@Data
+public class Student {
+
+    public String name;
+    public int age;
+    public String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return ("Student's name is: " + this.getName() +
+                "Student age is: " + this.getAge() +
+                "Student address is: " + this.getAddress());
+    }
+
+    public static void main(String[] args) {
+        Student john = new Student("John", 21, "1234 E. king's NY");
+        System.out.println(john);
+
+    }
+}
+
+
