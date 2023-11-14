06258a0b82dc99e974cfb1183faccd5d13041848
gfizzle <winstongdouglas4@gmail.com>
2023-11-13 23:16:52 -0600

    second

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//        for (int i = 1; i <= 5; i++) {
 
             // Press Ctrl+D to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+//            System.out.println("i = " + i);
+//        }
     }
 }
